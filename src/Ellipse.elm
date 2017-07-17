dafe11b3d5f5e1548d296cea9ab16c2740c67424
Folkert de Vries <folkert@folkertdev.nl>
2017-07-17 15:53:06 +0200

    add segment

---
+++ src/Ellipse.elm
@@ -0,0 +1,351 @@
+module Ellipse
+    exposing
+        ( CenterParameterization
+        , EndpointParameterization
+        , centerToEndpoint
+        , endpointToCenter
+        , at
+        , atAngle
+        , approximateArcLength
+        , mod2pi
+        , tau
+        , validateRadii
+        )
+
+import Path exposing (ArcFlag, Direction, clockwise, counterClockwise, smallestArc, largestArc)
+import Vector2 exposing (..)
+import Matrix2 exposing (..)
+
+
+tau : Float
+tau =
+    2 * pi
+
+
+type alias EndpointParameterization =
+    { start : Vec2 Float
+    , end : Vec2 Float
+    , radii : ( Float, Float )
+    , xAxisRotate : Float
+    , arcFlag : ArcFlag
+    , direction : Direction
+    }
+
+
+type alias CenterParameterization =
+    { center : Vec2 Float
+    , radii : ( Float, Float )
+    , startAngle : Float
+    , endAngle : Float
+    , xAxisRotate : Float
+    , arcFlag : ArcFlag
+    , direction : Direction
+    }
+
+
+validateRadii : EndpointParameterization -> EndpointParameterization
+validateRadii ({ radii } as parameterization) =
+    let
+        ( x1_, y1_ ) =
+            coordinatePrime parameterization
+
+        ( rx, ry ) =
+            radii
+
+        v =
+            (x1_ ^ 2 / rx ^ 2 + y1_ ^ 2 / ry ^ 2)
+    in
+        if v <= 1 then
+            parameterization
+        else
+            { parameterization | radii = ( sqrt v * rx, sqrt v * ry ) }
+
+
+normalize : CenterParameterization -> CenterParameterization
+normalize ({ startAngle, endAngle, xAxisRotate } as parameterization) =
+    { parameterization
+        | startAngle = mod2pi (startAngle - xAxisRotate)
+        , endAngle = mod2pi (endAngle - xAxisRotate)
+        , xAxisRotate = 0
+    }
+
+
+atAngle : Float -> CenterParameterization -> Vec2 Float
+atAngle theta { xAxisRotate, center, radii } =
+    ( cos theta, sin theta )
+        |> Vector2.map2 (*) radii
+        |> Matrix2.mulVector (conversionMatrix xAxisRotate)
+        |> Vector2.add center
+
+
+approximateArcLength : { a | minDepth : Int, error : Float } -> EndpointParameterization -> Float
+approximateArcLength { minDepth, error } parameterization =
+    let
+        midPoint =
+            at 0.5 (endpointToCenter parameterization)
+
+        { start, end } =
+            parameterization
+
+        length =
+            Vector2.distance start end
+
+        length2 =
+            Vector2.distance start midPoint + Vector2.distance midPoint end
+    in
+        if minDepth > 0 || length2 - length > error then
+            approximateArcLength { minDepth = minDepth - 1, error = error } { parameterization | end = midPoint }
+                + approximateArcLength { minDepth = minDepth - 1, error = error } { parameterization | start = midPoint }
+        else
+            length2
+
+
+at : Float -> CenterParameterization -> Vec2 Float
+at t ({ startAngle, endAngle } as parameterization) =
+    atAngle (startAngle + t * (mod2pi <| endAngle - startAngle)) parameterization
+
+
+mod2pi : Float -> Float
+mod2pi x =
+    x - toFloat (truncate (x / (2 * pi))) * 2 * pi
+
+
+{-| Approximation of the circumference of the full ellipse
+
+Using Approximation 3 from [this post](https://www.mathsisfun.com/geometry/ellipse-perimeter.html)
+-}
+circumference : { a | radii : ( Float, Float ) } -> Float
+circumference { radii } =
+    let
+        ( a, b ) =
+            radii
+
+        h =
+            ((a - b) ^ 2) / ((a + b) ^ 2)
+
+        circumference =
+            pi * (a + b) * (1 + (3 * h) / (10 + sqrt (4 - 3 * h)))
+    in
+        circumference
+
+
+arcSegmentLength : EndpointParameterization -> Float
+arcSegmentLength =
+    arcSegmentLengthHelper << normalize << endpointToCenter
+
+
+arcSegmentLengthHelper : CenterParameterization -> Float
+arcSegmentLengthHelper parameterization =
+    let
+        --  { start , end } = centerToEndpoint parameterization
+        helper start end angleStart angleEnd =
+            Vector2.distance start end / (2 * sin ((angleStart - angleEnd) / 2)) * (angleStart - angleEnd)
+
+        startAngle =
+            min parameterization.startAngle parameterization.endAngle
+                |> mod2pi
+
+        endAngle =
+            max parameterization.startAngle parameterization.endAngle
+                |> mod2pi
+
+        recurse v =
+            arcSegmentLengthHelper { parameterization | endAngle = tau / 2, startAngle = startAngle }
+                + arcSegmentLengthHelper { parameterization | startAngle = tau / 2, endAngle = endAngle }
+    in
+        {-
+           if startAngle < (tau / 2) && endAngle > (tau / 2) then
+               recurse (tau * 0.25)
+           else if startAngle < (tau / 4) && endAngle > (tau / 4) then
+               recurse (tau * 0.5)
+           else if startAngle < (tau * 0.75) && endAngle > (tau * 0.75) then
+               recurse (tau * 0.75)
+           else
+        -}
+        let
+            { start, end } =
+                centerToEndpoint parameterization
+        in
+            helper start end startAngle endAngle
+
+
+conversionMatrix : Float -> Mat2 Float
+conversionMatrix xAxisRotate =
+    ( ( cos xAxisRotate, -1 * sin xAxisRotate )
+    , ( sin xAxisRotate, cos xAxisRotate )
+    )
+
+
+inverseConversionMatrix : Float -> Mat2 Float
+inverseConversionMatrix xAxisRotate =
+    ( ( cos xAxisRotate, sin xAxisRotate )
+    , ( -1 * sin xAxisRotate, cos xAxisRotate )
+    )
+
+
+centerToEndpoint : CenterParameterization -> EndpointParameterization
+centerToEndpoint { center, radii, startAngle, endAngle, xAxisRotate } =
+    let
+        conversion =
+            conversionMatrix xAxisRotate
+
+        deltaTheta =
+            endAngle - startAngle
+
+        ( rx, ry ) =
+            radii
+
+        p1 =
+            ( rx * cos startAngle, ry * sin startAngle )
+                |> Matrix2.mulVector conversion
+                |> Vector2.add center
+
+        p2 =
+            ( rx * cos endAngle, ry * sin endAngle )
+                |> Matrix2.mulVector conversion
+                |> Vector2.add center
+
+        arcFlag =
+            if abs deltaTheta > pi then
+                largestArc
+            else
+                smallestArc
+
+        sweepFlag =
+            if deltaTheta > 0 then
+                clockwise
+            else
+                counterClockwise
+    in
+        { start = p1, end = p2, radii = radii, arcFlag = arcFlag, direction = sweepFlag, xAxisRotate = xAxisRotate }
+
+
+coordinatePrime : EndpointParameterization -> Vec2 Float
+coordinatePrime { start, end, xAxisRotate } =
+    let
+        rotate =
+            inverseConversionMatrix xAxisRotate
+
+        ( x1_, y1_ ) =
+            Vector2.sub start end
+                |> Vector2.divideBy 2
+                |> Matrix2.mulVector rotate
+
+        ( x1, y1 ) =
+            start
+
+        ( x2, y2 ) =
+            end
+    in
+        ( x1_, y1_ )
+
+
+endpointToCenter : EndpointParameterization -> CenterParameterization
+endpointToCenter ({ start, end, radii, xAxisRotate, arcFlag, direction } as parameterization) =
+    let
+        ( rx, ry ) =
+            radii
+
+        ( x1_, y1_ ) =
+            coordinatePrime parameterization
+
+        sign =
+            if arcFlag == largestArc && direction == clockwise || arcFlag == smallestArc && direction == counterClockwise then
+                -1
+            else
+                1
+
+        numerator =
+            (rx ^ 2 * ry ^ 2)
+                - (rx ^ 2 * y1_ ^ 2)
+                - (ry ^ 2 * x1_ ^ 2)
+
+        denominator =
+            (rx ^ 2 * y1_ ^ 2)
+                + (ry ^ 2 * x1_ ^ 2)
+
+        root =
+            if denominator == 0 then
+                0
+            else
+                sign * sqrt (numerator / denominator)
+
+        center_ =
+            ( ((rx * y1_) / ry) * root
+            , (-1 * ((ry * x1_) / rx)) * root
+            )
+
+        center =
+            center_
+                |> Matrix2.mulVector (conversionMatrix xAxisRotate)
+                |> Vector2.add
+                    (Vector2.add start end
+                        |> Vector2.divideBy 2
+                    )
+
+        p1 =
+            ( x1_, y1_ )
+
+        positive angle =
+            if angle < 0 then
+                positive (angle + 2 * pi)
+            else
+                angle
+
+        startAngle =
+            let
+                temp =
+                    Vector2.sub p1 center_
+                        |> flip (Vector2.map2 (/)) radii
+                        |> angle ( 1, 0 )
+            in
+                (if direction == counterClockwise && deltaTheta > 0 then
+                    temp - tau
+                 else if direction == clockwise && deltaTheta < 0 then
+                    temp + tau
+                 else
+                    temp
+                )
+                    |> positive
+
+        deltaTheta =
+            let
+                first =
+                    Vector2.map2 (/) (Vector2.sub p1 center_) radii
+
+                second =
+                    Vector2.map2 (/) (Vector2.sub (Vector2.negate p1) center_) radii
+            in
+                angle first second
+    in
+        { center = center
+        , xAxisRotate = xAxisRotate
+        , startAngle = startAngle |> mod2pi
+        , endAngle = startAngle + deltaTheta |> mod2pi
+        , arcFlag = arcFlag
+        , direction = direction
+        , radii = radii
+        }
+
+
+{-| A signed angle between two vectors (the standard implementation is unsigned)
+-}
+angle : Vec2 Float -> Vec2 Float -> Float
+angle u v =
+    let
+        ( ux, uy ) =
+            u
+
+        ( vx, vy ) =
+            v
+
+        sign =
+            if ux * vy - uy * vx < 0 then
+                -1
+            else
+                1
+
+        q =
+            acos (Vector2.dot u v / (Vector2.length u * Vector2.length v))
+    in
+        sign * abs q
---
+++ src/Segment.elm
@@ -0,0 +1,290 @@
+module Segment exposing (fromPath, toPath, Segment(..), length, lengthWithOptions, at)
+
+{-| An alternative interpretation of paths that is convenient for mathematical operations.
+
+Here, a path is viewed as a list of segments with a start and end point.
+
+@docs Segment
+
+@docs fromPath, toPath
+
+# Operations
+@docs at, length, lengthWithOptions
+-}
+
+import Path exposing (..)
+import Vector2 exposing (Vec2, Float2)
+import Ellipse
+import CubicBezier exposing (..)
+import List.Extra as List
+
+
+{-| The four types of segments.
+
+For segments, the `xAxisRotate` field is in radians.
+-}
+type Segment
+    = LineSegment Coordinate Coordinate
+    | Quadratic Coordinate Coordinate Coordinate
+    | Cubic Coordinate Coordinate Coordinate Coordinate
+    | Arc
+        { start : Coordinate
+        , end : Coordinate
+        , radii : ( Float, Float )
+        , xAxisRotate : Float
+        , arcFlag : ArcFlag
+        , direction : Direction
+        }
+
+
+toDrawTo : Segment -> DrawTo
+toDrawTo segment =
+    case segment of
+        LineSegment start end ->
+            lineTo [ end ]
+
+        Quadratic start c1 end ->
+            quadraticCurveTo [ ( c1, end ) ]
+
+        Cubic start c1 c2 end ->
+            cubicCurveTo [ ( c1, c2, end ) ]
+
+        Arc { end, radii, xAxisRotate, arcFlag, direction } ->
+            EllipticalArc [ { target = end, radii = radii, xAxisRotate = degrees xAxisRotate, arcFlag = arcFlag, direction = direction } ]
+
+
+{-| Convert a list of segments to a path
+
+It is assumed that for every two adjacent segments in the list, the first segment's end point is the second segment's starting point
+-}
+toPath : List Segment -> Path
+toPath segments =
+    case segments of
+        [] ->
+            []
+
+        segment :: rest ->
+            [ subpath (moveTo (startingPoint segment)) (List.map toDrawTo segments) ]
+
+
+{-| Create segments from a path
+-}
+fromPath : Path -> List Segment
+fromPath =
+    List.concatMap subpathToSegments
+
+
+subpathToSegments : SubPath -> List Segment
+subpathToSegments { moveto, drawtos } =
+    case moveto of
+        MoveTo coordinate ->
+            let
+                cursorState =
+                    { start = coordinate, cursor = coordinate }
+
+                -- fake segment to get the fold going, not part of the final result
+                initial =
+                    LineSegment coordinate coordinate
+
+                folder drawto ( previousSegment, accum ) =
+                    let
+                        newSegments =
+                            toSegment previousSegment drawto
+
+                        finalNewSegment =
+                            Maybe.withDefault previousSegment (List.last newSegments)
+                    in
+                        ( finalNewSegment, newSegments ++ accum )
+            in
+                traverse folder initial drawtos
+
+
+startingPoint : Segment -> Vec2 Float
+startingPoint segment =
+    case segment of
+        LineSegment _ p ->
+            p
+
+        Quadratic _ _ p ->
+            p
+
+        Cubic _ _ _ p ->
+            p
+
+        Arc { start } ->
+            start
+
+
+toSegment : Segment -> DrawTo -> List Segment
+toSegment previous drawto =
+    let
+        start =
+            startingPoint previous
+
+        ( startX, startY ) =
+            start
+    in
+        case drawto of
+            LineTo coordinates ->
+                List.map2 LineSegment (start :: coordinates) coordinates
+
+            Horizontal xs ->
+                let
+                    coordinates =
+                        List.map (\x -> ( x, startY )) xs
+                in
+                    List.map2 LineSegment (start :: coordinates) coordinates
+
+            Vertical ys ->
+                let
+                    coordinates =
+                        List.map (\y -> ( startX, y )) ys
+                in
+                    List.map2 LineSegment (start :: coordinates) coordinates
+
+            CurveTo coordinates ->
+                let
+                    folder ( c1, c2, p ) ( segmentStart, accum ) =
+                        ( p, Cubic segmentStart c1 c2 p :: accum )
+                in
+                    traverse folder start coordinates
+
+            SmoothCurveTo coordinates ->
+                let
+                    controlPoint =
+                        case previous of
+                            Cubic _ _ ( cx, cy ) _ ->
+                                -- reflect the previous control point in the end point
+                                Vector2.add start ( startX - cx, startY - cy )
+
+                            _ ->
+                                start
+
+                    folder ( c2, p ) ( ( c1, segmentStart ), accum ) =
+                        ( ( c2, p ), Cubic segmentStart c1 c2 p :: accum )
+                in
+                    traverse folder ( controlPoint, start ) coordinates
+
+            QuadraticBezierCurveTo coordinates ->
+                let
+                    folder ( c, p ) ( segmentStart, accum ) =
+                        ( p, Quadratic segmentStart c p :: accum )
+                in
+                    traverse folder start coordinates
+
+            SmoothQuadraticBezierCurveTo coordinates ->
+                let
+                    controlPoint =
+                        case previous of
+                            Quadratic _ ( cx, cy ) _ ->
+                                ( cx, cy )
+
+                            _ ->
+                                start
+
+                    folder point ( ( previousControlPoint, segmentStart ), accum ) =
+                        let
+                            controlPoint =
+                                -- reflect c in segmentStart
+                                Vector2.sub segmentStart previousControlPoint
+                                    |> Vector2.add point
+                        in
+                            ( ( controlPoint, point ), Quadratic segmentStart controlPoint point :: accum )
+                in
+                    traverse folder ( controlPoint, start ) coordinates
+
+            EllipticalArc arguments ->
+                let
+                    folder args ( segmentStart, accum ) =
+                        ( args.target
+                        , Arc
+                            { start = segmentStart
+                            , end = args.target
+                            , radii = args.radii
+                            , xAxisRotate = radians args.xAxisRotate
+                            , arcFlag = args.arcFlag
+                            , direction = args.direction
+                            }
+                            :: accum
+                        )
+                in
+                    traverse folder start arguments
+
+            ClosePath ->
+                []
+
+
+traverse : (a -> ( b, List c ) -> ( b, List c )) -> b -> List a -> List c
+traverse folder initial elements =
+    List.foldl folder ( initial, [] ) elements
+        |> Tuple.second
+        |> List.reverse
+
+
+{-| Get the location at a point on the curve, only defined in the range [0, 1].
+-}
+at : Float -> Segment -> ( Float, Float )
+at t segment =
+    case segment of
+        LineSegment start end ->
+            Vector2.directionFromTo start end
+                |> Vector2.scale t
+
+        Quadratic start c1 end ->
+            CubicBezier.fromQuadratic start c1 end
+                |> CubicBezier.at t
+
+        Cubic start c1 c2 end ->
+            CubicBezier.fromPoints start c1 c2 end
+                |> CubicBezier.at t
+
+        Arc parameterization ->
+            Ellipse.at t (Ellipse.endpointToCenter parameterization)
+
+
+{-| Get the derivative at a point on the curve, only defined in the range [0, 1].
+-}
+derivativeAt : Float -> Segment -> Maybe ( Float, Float )
+derivativeAt t segment =
+    Debug.crash ""
+
+
+{-| The approximate length of a segment
+-}
+length : Segment -> Float
+length =
+    lengthWithOptions { minDepth = 10, error = 1.0e-12 }
+
+
+{-| Supply the options for the approximation
+
+* `minDepth`: Minimum recursion depth for calculating the length of arc segments. Default is `10`.
+* `error`: Minimal amount of progress that an recursive step must make. Default is `1.0e-12`.
+-}
+lengthWithOptions : { minDepth : Int, error : Float } -> Segment -> Float
+lengthWithOptions config segment =
+    case segment of
+        LineSegment ( x0, y0 ) ( x1, y1 ) ->
+            let
+                ( dx, dy ) =
+                    ( x0 - x1, y0 - y1 )
+            in
+                sqrt (dx * dx + dy * dy)
+
+        Quadratic start c1 end ->
+            CubicBezier.fromQuadratic start c1 end
+                |> arcLength 1.0 config.error
+
+        Cubic start c1 c2 end ->
+            CubicBezier.fromPoints start c1 c2 end
+                |> arcLength 1.0 config.error
+
+        Arc args ->
+            Ellipse.approximateArcLength config args
+
+
+{-| Give the (x,y) locations of the intersections between two segments
+-}
+intersections : Segment -> Segment -> List ( Float, Float )
+intersections segment1 segment2 =
+    Debug.crash ""
