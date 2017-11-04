560ff29a668e9d0c79e886b67a1cc56cd413739d
Folkert de Vries <folkert@folkertdev.nl>
2017-11-04 18:18:25 +0100

    improve Path documentation

--- src/Path.elm
+++ src/Path.elm
@@ -2,66 +2,15 @@ module Path
     exposing
         ( Path
         , element
-        , mapCoordinate
-        , mapWithCursorState
         , parse
         , toString
         )
 
-{-| Low-level module for constructing svg paths.
+{-| Module for layering SubPaths into Paths.
 
-This module provides a wrapper around the svg path syntax. A path can be parsed from a string or build up using the
-svg path primitives, and then converted to a string that can be used in the [`d` attribute][d-attribute] to render the path.
+Most of the interesting stuff happens in the `SubPath` and `Curve` modules.
+`Path` is simply for combining multiple subpaths into one string or element.
 
-Note that this is not the most convenient way of drawing. This package is mainly meant as a primitive to build other packages on top of.
-If you want to visualize data, have a look at [elm-plot] and [elm-visualization]. If you want to draw geometry, check out [opensolid].
-
-For more information on svg paths, see the [MDN documentation].
-
-[MDN documentation]: https://developer.mozilla.org/en/docs/Web/SVG/Tutorial/MixedPaths.
-[elm-plot]: http://package.elm-lang.org/packages/terezka/elm-plot/latest
-[elm-visualization]: http://package.elm-lang.org/packages/gampleman/elm-visualization/latest
-[opensolid]: http://package.elm-lang.org/packages/opensolid/geometry/latest
-[d-attribute]: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d
-
-[`MoveTo`]: #MoveTo
-[`DrawTo`]: #DrawTo
-[with the spec]: https://www.w3.org/TR/SVG/paths.html#PathDataMovetoCommands
-
-
-## Data Layout
-
-A path is a list of subpaths that are drawn in order. A subpath consists of a [`MoveTo`] instruction followed by a list of [`DrawTo`] instructions.
-
-This package only supports absolute coordinates and instructions, but it is possible to parse strings with relative intructions.
-When a path is parsed, the first [`MoveTo`] instruction is always interpreted as absolute (this is in accordance [with the spec]),
-thus making sure that there is always an absolute cursor position.
-
-The constructors are exposed, so if you need an escape hatch it is available. As always though, never reach for it when there are other options.
-
-The vector types are from [Zinggi/elm-webgl-math](http://package.elm-lang.org/packages/Zinggi/elm-webgl-math/latest). They are just type aliases for tuples.
-
-
-## Example
-
-    myPoints : List ( Float, Float )
-
-
-    -- connect all the points with a straight line
-
-    linear : List (Vec2 Float) -> Path
-    linear points =
-        case points of
-            [] ->
-                []
-
-            p :: ps ->
-                [ subpath (moveTo p) [ lineTo ps ] ]
-
-    main =
-        Svg.svg [ width "400", height "400" ]
-            [ Path.element (linear myPoints) [ fill "none" ]
-            ]
 
 
 ## Data Structures
@@ -78,20 +27,19 @@ The vector types are from [Zinggi/elm-webgl-math](http://package.elm-lang.org/pa
 
 @docs element, toString
 
-
-## Modifying Paths
-
-@docs mapCoordinate, mapWithCursorState
-
 -}
 
-import LowLevel.Command as Command exposing (CursorState, DrawTo)
 import Parser
 import Path.LowLevel.Parser as PathParser
 import SubPath exposing (SubPath, subpath)
 import Svg
 import Svg.Attributes
-import Vector2 as Vec2 exposing (Vec2)
+
+
+{-| A path is a list of [`SubPath`](#subpath)s.
+-}
+type alias Path =
+    List SubPath
 
 
 {-| Construct an svg path element from a `Path` with the given attributes
@@ -101,16 +49,25 @@ element path attributes =
     Svg.path (Svg.Attributes.d (toString path) :: attributes) []
 
 
-{-| A path is a list of [`subpath`](#subpath)s.
--}
-type alias Path =
-    List SubPath
+{-| Turn a `Path` into a `String`. The result is ready to be used with the `d` attribute.
 
 
-{-| Turn a `MixedPath` into a `String`. The result is ready to be used with the `d` attribute.
+    myPath : SubPath
+    myPath =
+        subpath (moveTo (0,0)) [ lineTo [ (42, 73) ] ]
 
-    Path.toString [ subpath (moveTo (0,0)) [ lineBy ( 42, 73 ) ] ]
-        --> "M0,0 l42,73"
+
+    Path.toString [ myPath ]
+        --> "M0,0 L42,73"
+
+    -- forms a cycle (almost isomorphism) with parse
+    arc : String
+    arc = "M80,230 A45,45 90 0 1 125,275"
+
+    arc
+        |> parse
+        |> Result.map Path.toString
+        --> Ok arc
 
 -}
 toString : Path -> String
@@ -120,41 +77,23 @@ toString =
 
 {-| Parse a path string into a `Path`
 
-    parse "M0,0 l42,73"
-        --> Ok [{ moveto = MoveTo Absolute (0,0), drawtos = [ LineTo Relative  [(42, 73)]]}]
+    import SubPath exposing (SubPath, subpath)
+    import LowLevel.Command exposing (moveTo, lineTo)
 
-Only accepts valid complete subpaths (a sequences of a move followed by zero or more draws). Relative instructions are converted to absolute ones.
-The types and constructors in the output are described [here](#internal-data-used-by-the-parser-).
+    expected : SubPath
+    expected =
+        subpath (moveTo (0,0)) [ lineTo [ (42, 73) ] ]
+
+    parse "M0,0 l42,73"
+        --> Ok [expected]
+
+
+Only accepts valid complete subpaths (a sequences of a move followed by zero or more draws). Relative instructions are converted to absolute ones. Short-hand curve extensions are converted to explicit curve instructions.
 
 The parser uses [`elm-tools/parser`](http://package.elm-lang.org/packages/elm-tools/parser/2.0.1/).
 The error type is [`Parser.Error`](http://package.elm-lang.org/packages/elm-tools/parser/2.0.1/Parser#Error).
 
 -}
-parse : String -> Result Parser.Error (List SubPath)
+parse : String -> Result Parser.Error Path
 parse =
     Result.map SubPath.fromLowLevel << PathParser.parse
-
-
-{-| Manipulate the coordinates in your SVG. This can be useful for scaling the svg.
-
-    -- make the image twice as big in the x direction
-    [ subpath (moveTo (10,0)) [ lineTo [ (42, 42) ] ] ]
-        |> mapCoordinate (\(x,y) -> (2 * x, y))
-             --> [ subpath (moveTo (20,0)) [ lineTo [ (84, 42) ] ] ]
-
--}
-mapCoordinate : (Vec2 Float -> Vec2 Float) -> Path -> Path
-mapCoordinate f path =
-    List.map (SubPath.mapCoordinate f) path
-
-
-{-| Map over the `DrawTo`s in a path with access to their starting point.
-
-Many mathematical operations (length, derivative, curvature) are only possible when a segment is fully specified. A `DrawTo` on its
-own misses its starting point - the current cursor position. This function makes the cursor position and the start of the current subpath available
-when mapping.
-
--}
-mapWithCursorState : (CursorState -> DrawTo -> b) -> Path -> List b
-mapWithCursorState f =
-    List.concatMap (SubPath.mapWithCursorState f)
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -27,8 +27,8 @@ toStringTests =
                 \_ ->
                     SubPath.toString subpath |> Expect.equal expected
     in
-    describe "pretty printing tests" <|
-        List.map (uncurry createTest) cases
+        describe "pretty printing tests" <|
+            List.map (uncurry createTest) cases
 
 
 pathCommand =
@@ -42,7 +42,7 @@ pathTests =
         [ fuzz2 fuzzCoordinate (Fuzz.list fuzzCoordinate) "mapWithCursorState" <|
             \start rest ->
                 [ SubPath.subpath (Command.moveTo start) (List.map (Command.lineTo << List.singleton) rest) ]
-                    |> Path.mapWithCursorState (\{ cursor } _ -> cursor)
+                    |> List.concatMap (SubPath.mapWithCursorState (\{ cursor } _ -> cursor))
                     |> Expect.equal
                         (if rest /= [] then
                             start :: List.take (List.length rest - 1) rest
@@ -73,143 +73,143 @@ toAbsoluteConversion =
         fromLowLevelDrawTo state drawto =
             Command.fromLowLevelDrawTo drawto state
     in
-    describe "convert commands to absolute"
-        [ test "lineto relative" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.LineTo Relative [ ( 10, 10 ) ])
-                    |> Expect.equal
-                        (Just ( LineTo [ ( 110, 110 ) ], { startConfig | cursor = ( 110, 110 ) } ))
-        , test "lineto absolute" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.LineTo Absolute [ ( 10, 10 ) ])
-                    |> Expect.equal (Just ( LineTo [ ( 10, 10 ) ], { startConfig | cursor = ( 10, 10 ) } ))
-        , test "horizontal relative" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.Horizontal Relative [ 10 ])
-                    |> Expect.equal (Just ( LineTo [ ( 110, 100 ) ], { startConfig | cursor = ( 110, 100 ) } ))
-        , test "horizontal absolute" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.Horizontal Absolute [ 10 ])
-                    |> Expect.equal (Just ( LineTo [ ( 10, 0 ) ], { startConfig | cursor = ( 10, 100 ) } ))
-        , test "vertical relative" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.Vertical Relative [ 10 ])
-                    |> Expect.equal (Just ( LineTo [ ( 100, 110 ) ], { startConfig | cursor = ( 100, 110 ) } ))
-        , test "vertical absolute" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.Vertical Absolute [ 10 ])
-                    |> Expect.equal (Just ( LineTo [ ( 0, 10 ) ], { startConfig | cursor = ( 100, 10 ) } ))
-        , test "curveTo relative" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.CurveTo Relative [ ( ( 0, 5 ), ( 10, 5 ), ( 10, 10 ) ) ])
-                    |> Expect.equal
-                        (Just
-                            ( CurveTo [ ( ( 100, 105 ), ( 110, 105 ), ( 110, 110 ) ) ]
-                            , { startConfig | cursor = ( 110, 110 ), previousControlPoint = Just ( 110, 105 ) }
+        describe "convert commands to absolute"
+            [ test "lineto relative" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.LineTo Relative [ ( 10, 10 ) ])
+                        |> Expect.equal
+                            (Just ( LineTo [ ( 110, 110 ) ], { startConfig | cursor = ( 110, 110 ) } ))
+            , test "lineto absolute" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.LineTo Absolute [ ( 10, 10 ) ])
+                        |> Expect.equal (Just ( LineTo [ ( 10, 10 ) ], { startConfig | cursor = ( 10, 10 ) } ))
+            , test "horizontal relative" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.Horizontal Relative [ 10 ])
+                        |> Expect.equal (Just ( LineTo [ ( 110, 100 ) ], { startConfig | cursor = ( 110, 100 ) } ))
+            , test "horizontal absolute" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.Horizontal Absolute [ 10 ])
+                        |> Expect.equal (Just ( LineTo [ ( 10, 0 ) ], { startConfig | cursor = ( 10, 100 ) } ))
+            , test "vertical relative" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.Vertical Relative [ 10 ])
+                        |> Expect.equal (Just ( LineTo [ ( 100, 110 ) ], { startConfig | cursor = ( 100, 110 ) } ))
+            , test "vertical absolute" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.Vertical Absolute [ 10 ])
+                        |> Expect.equal (Just ( LineTo [ ( 0, 10 ) ], { startConfig | cursor = ( 100, 10 ) } ))
+            , test "curveTo relative" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.CurveTo Relative [ ( ( 0, 5 ), ( 10, 5 ), ( 10, 10 ) ) ])
+                        |> Expect.equal
+                            (Just
+                                ( CurveTo [ ( ( 100, 105 ), ( 110, 105 ), ( 110, 110 ) ) ]
+                                , { startConfig | cursor = ( 110, 110 ), previousControlPoint = Just ( 110, 105 ) }
+                                )
                             )
-                        )
-        , test "curveTo absolute" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.CurveTo Absolute [ ( ( 0, 5 ), ( 10, 5 ), ( 10, 10 ) ) ])
-                    |> Expect.equal
-                        (Just
-                            ( CurveTo [ ( ( 0, 5 ), ( 10, 5 ), ( 10, 10 ) ) ]
-                            , { startConfig | cursor = ( 10, 10 ), previousControlPoint = Just ( 10, 5 ) }
+            , test "curveTo absolute" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.CurveTo Absolute [ ( ( 0, 5 ), ( 10, 5 ), ( 10, 10 ) ) ])
+                        |> Expect.equal
+                            (Just
+                                ( CurveTo [ ( ( 0, 5 ), ( 10, 5 ), ( 10, 10 ) ) ]
+                                , { startConfig | cursor = ( 10, 10 ), previousControlPoint = Just ( 10, 5 ) }
+                                )
                             )
-                        )
-        , test "smoothCurveTo relative" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.SmoothCurveTo Relative [ ( ( 10, 5 ), ( 10, 10 ) ) ])
-                    |> Expect.equal
-                        (Just
-                            ( CurveTo [ ( ( 100, 100 ), ( 110, 105 ), ( 110, 110 ) ) ]
-                            , { startConfig | cursor = ( 110, 110 ), previousControlPoint = Just ( 110, 105 ) }
+            , test "smoothCurveTo relative" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.SmoothCurveTo Relative [ ( ( 10, 5 ), ( 10, 10 ) ) ])
+                        |> Expect.equal
+                            (Just
+                                ( CurveTo [ ( ( 100, 100 ), ( 110, 105 ), ( 110, 110 ) ) ]
+                                , { startConfig | cursor = ( 110, 110 ), previousControlPoint = Just ( 110, 105 ) }
+                                )
                             )
-                        )
-        , test "smoothCurveTo absolute" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.SmoothCurveTo Absolute [ ( ( 10, 5 ), ( 10, 10 ) ) ])
-                    |> Expect.equal
-                        (Just
-                            ( CurveTo [ ( ( 100, 100 ), ( 10, 5 ), ( 10, 10 ) ) ]
-                            , { startConfig | cursor = ( 10, 10 ), previousControlPoint = Just ( 10, 5 ) }
+            , test "smoothCurveTo absolute" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.SmoothCurveTo Absolute [ ( ( 10, 5 ), ( 10, 10 ) ) ])
+                        |> Expect.equal
+                            (Just
+                                ( CurveTo [ ( ( 100, 100 ), ( 10, 5 ), ( 10, 10 ) ) ]
+                                , { startConfig | cursor = ( 10, 10 ), previousControlPoint = Just ( 10, 5 ) }
+                                )
                             )
-                        )
-        , test "quadraticBezierCurveTo relative" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.QuadraticBezierCurveTo Relative [ ( ( 10, 5 ), ( 10, 10 ) ) ])
-                    |> Expect.equal
-                        (Just
-                            ( QuadraticBezierCurveTo [ ( ( 110, 105 ), ( 110, 110 ) ) ]
-                            , { startConfig | cursor = ( 110, 110 ), previousControlPoint = Just ( 110, 105 ) }
+            , test "quadraticBezierCurveTo relative" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.QuadraticBezierCurveTo Relative [ ( ( 10, 5 ), ( 10, 10 ) ) ])
+                        |> Expect.equal
+                            (Just
+                                ( QuadraticBezierCurveTo [ ( ( 110, 105 ), ( 110, 110 ) ) ]
+                                , { startConfig | cursor = ( 110, 110 ), previousControlPoint = Just ( 110, 105 ) }
+                                )
                             )
-                        )
-        , test "quadraticBezierCurveTo absolute" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.QuadraticBezierCurveTo Absolute [ ( ( 10, 5 ), ( 10, 10 ) ) ])
-                    |> Expect.equal
-                        (Just
-                            ( QuadraticBezierCurveTo [ ( ( 10, 5 ), ( 10, 10 ) ) ]
-                            , { startConfig | cursor = ( 10, 10 ), previousControlPoint = Just ( 10, 5 ) }
+            , test "quadraticBezierCurveTo absolute" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.QuadraticBezierCurveTo Absolute [ ( ( 10, 5 ), ( 10, 10 ) ) ])
+                        |> Expect.equal
+                            (Just
+                                ( QuadraticBezierCurveTo [ ( ( 10, 5 ), ( 10, 10 ) ) ]
+                                , { startConfig | cursor = ( 10, 10 ), previousControlPoint = Just ( 10, 5 ) }
+                                )
                             )
-                        )
-        , test "ellipticalArc relative" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.EllipticalArc Relative [ ellipticalArcExample ])
-                    |> Expect.equal (Just ( EllipticalArc [ { ellipticalArcExample | target = ( 110, 110 ) } ], { startConfig | cursor = ( 110, 110 ) } ))
-        , test "ellipticalArc absolute" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig (LowLevel.EllipticalArc Absolute [ ellipticalArcExample ])
-                    |> Expect.equal (Just ( EllipticalArc [ ellipticalArcExample ], { startConfig | cursor = ( 10, 10 ) } ))
-        , test "closepath" <|
-            \_ ->
-                fromLowLevelDrawTo startConfig LowLevel.ClosePath
-                    |> Expect.equal (Just ( ClosePath, { startConfig | cursor = startConfig.start } ))
-        , test "foldl/traverse works with multiple subpaths" <|
-            \_ ->
-                "M10,10 L15,15 m20,20"
-                    |> Path.parse
-                    |> Expect.equal
-                        (Ok
-                            [ SubPath.subpath (Command.MoveTo ( 10, 10 )) [ Command.LineTo [ ( 15, 15 ) ] ]
-                            , SubPath.subpath (Command.MoveTo ( 35, 35 )) []
-                            ]
-                        )
-        , test "finalPoint documentation example" <|
-            \_ ->
-                let
-                    finalPoint =
-                        Path.mapWithCursorState (flip Command.updateCursorState)
-                in
-                [ SubPath.subpath (Command.MoveTo ( 10, 10 )) [ Command.LineTo [ ( 15, 15 ) ] ]
-                , SubPath.subpath (Command.MoveTo ( 35, 35 )) []
-                ]
-                    |> finalPoint
-                    |> List.reverse
-                    |> List.head
-                    |> Maybe.map .cursor
-                    |> Expect.equal (Just ( 15, 15 ))
-        , fuzz fuzzMoveTo "a moveto always changes the CursorState starting position" <|
-            \(LowLevel.MoveTo mode coordinate) ->
-                let
-                    ( newCursor, newStart ) =
-                        if mode == Relative then
-                            ( Vec2.add startConfig.cursor coordinate
-                            , Vec2.add startConfig.start coordinate
+            , test "ellipticalArc relative" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.EllipticalArc Relative [ ellipticalArcExample ])
+                        |> Expect.equal (Just ( EllipticalArc [ { ellipticalArcExample | target = ( 110, 110 ) } ], { startConfig | cursor = ( 110, 110 ) } ))
+            , test "ellipticalArc absolute" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig (LowLevel.EllipticalArc Absolute [ ellipticalArcExample ])
+                        |> Expect.equal (Just ( EllipticalArc [ ellipticalArcExample ], { startConfig | cursor = ( 10, 10 ) } ))
+            , test "closepath" <|
+                \_ ->
+                    fromLowLevelDrawTo startConfig LowLevel.ClosePath
+                        |> Expect.equal (Just ( ClosePath, { startConfig | cursor = startConfig.start } ))
+            , test "foldl/traverse works with multiple subpaths" <|
+                \_ ->
+                    "M10,10 L15,15 m20,20"
+                        |> Path.parse
+                        |> Expect.equal
+                            (Ok
+                                [ SubPath.subpath (Command.MoveTo ( 10, 10 )) [ Command.LineTo [ ( 15, 15 ) ] ]
+                                , SubPath.subpath (Command.MoveTo ( 35, 35 )) []
+                                ]
                             )
-                        else
-                            ( coordinate
-                            , coordinate
-                            )
-                in
-                coordinate
-                    |> LowLevel.MoveTo mode
-                    |> flip Command.fromLowLevelMoveTo startConfig
-                    |> Expect.equal
-                        ( { startConfig | cursor = newCursor, start = newStart, previousControlPoint = Nothing }
-                        , MoveTo newCursor
-                        )
-        ]
+            , test "finalPoint documentation example" <|
+                \_ ->
+                    let
+                        finalPoint =
+                            List.concatMap (SubPath.mapWithCursorState (flip Command.updateCursorState))
+                    in
+                        [ SubPath.subpath (Command.MoveTo ( 10, 10 )) [ Command.LineTo [ ( 15, 15 ) ] ]
+                        , SubPath.subpath (Command.MoveTo ( 35, 35 )) []
+                        ]
+                            |> finalPoint
+                            |> List.reverse
+                            |> List.head
+                            |> Maybe.map .cursor
+                            |> Expect.equal (Just ( 15, 15 ))
+            , fuzz fuzzMoveTo "a moveto always changes the CursorState starting position" <|
+                \(LowLevel.MoveTo mode coordinate) ->
+                    let
+                        ( newCursor, newStart ) =
+                            if mode == Relative then
+                                ( Vec2.add startConfig.cursor coordinate
+                                , Vec2.add startConfig.start coordinate
+                                )
+                            else
+                                ( coordinate
+                                , coordinate
+                                )
+                    in
+                        coordinate
+                            |> LowLevel.MoveTo mode
+                            |> flip Command.fromLowLevelMoveTo startConfig
+                            |> Expect.equal
+                                ( { startConfig | cursor = newCursor, start = newStart, previousControlPoint = Nothing }
+                                , MoveTo newCursor
+                                )
+            ]
 
 
 fuzzMoveTo : Fuzzer LowLevel.MoveTo
